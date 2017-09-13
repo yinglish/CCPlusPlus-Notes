e2af08035fdf5b8f1203a2118aada181418c1587
yinglish <1226197359@qq.com>
2017-09-13 22:32:35 +0800

    saving

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+// 测试栈大小（）
+int main()
+{
+
+    //int a[2560000] = {0};
+    char *ch = NULL;
+    //cout << strlen(ch) << endl;
+    return 0;
+}
