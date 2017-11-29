a2d56ce074a79fbdeeec9db369f0758b0301ba37
yinglish <1226197359@qq.com>
2017-11-29 08:53:49 +0800

    save

--- 程序设计与算法（二）/week3/004.cpp
+++ 程序设计与算法（二）/week3/004.cpp
@@ -12,11 +12,11 @@ bool isZero(double num)
     return fabs(num - 0) < ESP;
 }
 
-double count24(int a[], int n)
+bool count24(int a[], int n)
 {
     if (n == 1)
     {
-        return isZero(a[0]);
+        return isZero(a[0] - 24);
     }
 
     int b[4];
@@ -26,12 +26,47 @@ double count24(int a[], int n)
         b[i] = a[i];
     }
 
+    b[i] = a[n - 2] + a[n - 1];
+    if (count24(b, n - 1))
+    {
+        return true;
+    }
 
+    b[i] = a[n - 2] - a[n - 1];
+    if (count24(b, n - 1))
+    {
+        return true;
+    }
 
+    b[i] = a[n - 1] * a[n - 2];
+    if (count24(b, n - 1))
+    {
+        return true;
+    }
 
+    if (!isZero(b[n - 2]))
+    {
+        if (count24(b, n - 1))
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 int main()
 {
+    while (cin >> a[0] >> a[1] >> a[2] >> a[3] && a[0] != -1)
+    {
+        if (count24(a, 4))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
     return 0;
 }
